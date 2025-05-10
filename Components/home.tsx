4607e6548e017fb599ada8467fb7efd11ba17912
NelsonJDAS <nelsondeabreu4@gmail.com>
2025-05-11 00:47:46 +0200

    fixing

--- Components/home.tsx
+++ Components/home.tsx
@@ -1,15 +1,11 @@
 import "../global.css"
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-4xl font-bold">Welcome to Quantia</h1>
-      <p className="mt-4 text-lg">Your AI-powered financial assistant</p>
-      <div className="mt-8">
-        <button className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600">
-          Get Started
-        </button>
-      </div>
-    </div>
+    <View className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Text className="text-4xl font-bold">Welcome to Quantia</Text>
+      <Text className="mt-4 text-lg mx-5">Mi queridisimo angel tienes que crear archivos tsx e importar el ../global.css :)</Text>
+    </View>
   );
 }
